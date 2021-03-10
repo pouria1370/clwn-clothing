cf04c18bd36dfaec7dc849b1d7a1585a9365e1fd
pouria1370 <kalantari_pouria@yahoo.com>
2021-03-10 10:13:55 -0800

    i just created first page with magnified styleing for hoverng

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import './homepage.styles.scss'
+import DirectoryMneu from '../../components/directory-menu-component/directory-component'
+
+
+const HomePage = () => (
+  <div className="homepage">
+    <DirectoryMneu /> 
+  </div>
+);
+export default HomePage;
